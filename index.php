aa318c08c2e90788a6fe10d1c766af273daa2232
lorenzorru0 <lorenzoorru999@gmail.com>
2021-10-20 16:50:50 +0200

    Fix navbar

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$nav = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologie', 'Domande frequenti'];
+
 $domande = [
     [
         "domanda" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
@@ -42,7 +44,9 @@ $domande = [
         "domanda" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
         "risposta" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ],
-]
+];
+
+$active = 4;
 
 ?>
 
@@ -80,11 +84,13 @@ $domande = [
         </div>
         <div class="headerBottom">
             <ul>
-                <li>Introduzione</li>
-                <li>Norme sulla privacy</li>
-                <li>Termini di servizio</li>
-                <li>Tecnologie</li>
-                <li class="active">Domande frequenti</li>
+                <?php foreach ($nav as $key => $link) { ?>
+                    <?php if ($active != $key) { ?>
+                        <li><?= $link ?></li>
+                    <?php } else { ?>
+                        <li class="active"><?= $link ?></li>
+                    <?php } ?>
+                <?php } ?>
             </ul>
         </div>
     </header>
@@ -101,7 +107,7 @@ $domande = [
     <footer>
         <div class="footerContainer">
             <p>Google - Tutto su Google - Privacy - Privacy - Termini</p>
-            <div> 
+            <div>
                 <select name="" id="">
                     <option value="it">italiano</option>
                 </select>
