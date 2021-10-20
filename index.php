a1ee9a23692354b7043777910fa041a3b4e57ee2
lorenzorru0 <lorenzoorru999@gmail.com>
2021-10-20 15:26:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Google font  -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+        <!-- /Google font -->
+        <title>Google FAQ</title>
+    </head>
+
+    <body>
+
+    <header>
+    
+    </header>
+
+    <main>
+    
+    </main>
+
+    <footer>
+    
+    </footer>
+
+    </body>
+
+</html>
